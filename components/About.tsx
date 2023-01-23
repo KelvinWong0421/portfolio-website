773823bde5bf495e99155f86e6998194e2955c78
KelvinWong0421 <kelvinwong159357@gmail.com>
2023-01-23 22:42:46 +0800

    update monkey v2

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import monkey from '../Image/monkey-technology.gif'
+import monkey from '../image/monkey-technology.gif'
 import Image from 'next/image'
 
 
