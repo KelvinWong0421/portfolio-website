f2f4364ec16fb2c8ff13121c9f508e371f17f9be
KelvinWong0421 <kelvinwong159357@gmail.com>
2023-05-20 20:46:30 +0800

    removeCircle

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -25,11 +25,6 @@ function Skill({direction,url}: Props) {
         filter group-hover:grayscale transition duration-500 ease-in-out ' 
         
         />
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white
-        h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
-            
-
-        </div>
         
     </div>
   )
