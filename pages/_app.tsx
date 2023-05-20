e4f7e537e068303f0babbf79e18b6d5eb6384eb3
KelvinWong0421 <kelvinwong159357@gmail.com>
2023-05-20 16:03:10 +0800

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,6 @@ import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-    <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=0.75, maximum-scale=0.75, minimum-scale=0.75, user-scalable=no" />
-    </Head>
     <Component {...pageProps} />
-    </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <body>
-        <Head/>
-        <Main />
+        <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=0.75, maximum-scale=0.75, minimum-scale=0.75, user-scalable=no" />
+        </Head>
+        <Main/>
         <NextScript />
       </body>
     </Html>
